2bdb0d537fa866d759f6dda7d6590a73a055542a
Bunmi Lawal <victoriaolawal05@gamil.com>
2023-03-23 10:49:14 -0500

    1st-commit

--- main.tf
+++ main.tf
@@ -2,16 +2,28 @@ provider "aws" {
   region = "us-west-1"
 }
 
-resource "aws_s3_bucket" "bootcamp30-123-vicky" {
-  bucket = "bootcamp30-123-vicky"
+resource "aws_vpc" "actions" {
+   cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name = "Class30"
+    Team = "DevOps"
+    Environment = "stage"
+    Division = "HR"
+  }
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
-  bucket = aws_s3_bucket.bootcamp30-123-vicky.id
-
-  # rule {
-  #   apply_server_side_encryption_by_default {
-  #     kms_master_key_id = aws_kms_key.mykey.arn
-  #     sse_algorithm     = "aws:kms"
+terraform {
+  required_version = "~> 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0" # Optional but recommended in production
     }
-  
+  }
+  backend "s3" {
+    bucket = "myotherclass26bucket"
+    key = "stage/terraform.tfstate"
+    region = "us-west-1"
+  }
+}
