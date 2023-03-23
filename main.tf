1039350e3b7a23e9a1675257c9c556f5030edf3c
Bunmi Lawal <victoriaolawal05@gamil.com>
2023-03-23 10:36:34 -0500

    second3

--- main.tf
+++ main.tf
@@ -9,10 +9,9 @@ resource "aws_s3_bucket" "bootcamp30-123-vicky" {
 resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
   bucket = aws_s3_bucket.bootcamp30-123-vicky.id
 
-  rule {
-    apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.mykey.arn
-      sse_algorithm     = "aws:kms"
+  # rule {
+  #   apply_server_side_encryption_by_default {
+  #     kms_master_key_id = aws_kms_key.mykey.arn
+  #     sse_algorithm     = "aws:kms"
     }
-  }
-}
+  
